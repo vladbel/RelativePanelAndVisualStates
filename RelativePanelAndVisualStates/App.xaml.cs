e108a6282f19986cde77a99516f95fb710118cdd
Vladimir Beliaev <vladbel@live.com>
2016-08-26 10:10:24 -0500

    Initial implementation

--- RelativePanelAndVisualStates/App.xaml.cs
+++ RelativePanelAndVisualStates/App.xaml.cs
@@ -72,7 +72,7 @@ namespace RelativePanelAndVisualStates
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Page2), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
---
+++ RelativePanelAndVisualStates/HeightToWidthConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace RelativePanelAndVisualStates
+{
+    public sealed class HeightToWidthConverter: IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ( value is double && (double)value != Double.NaN && (double)value > 0 )
+            {
+                return (double)value / 1.5;
+            }
+
+            return (double)100;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class WidthToHeightConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double && (double)value != Double.NaN && (double)value > 0)
+            {
+                return (double)value * 1.3;
+            }
+
+            return (double)100;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
